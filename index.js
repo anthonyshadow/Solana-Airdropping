628fabc82111db76466b63a926cbd06d2e162b2a
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-03-29 11:00:27 -0400

    setup wallet on devnet, write function to be able to check balance of the wallet

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const { Connection, PublicKey, clusterApiUrl, Keypair, LAMPORTS_PER_SOL } =  req
 
 const wallet = new Keypair()
 
-const publicKey = wallet._keypair.publicKey
+const publicKey = new PublicKey(wallet._keypair.publicKey)
 const secretKey = wallet._keypair.secretKey
 
-console.log(publicKey)
-console.log(secretKey)
+const getWalletBalance = async() => {
+    try{
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletballance = await connection.getBalance(publicKey)
+        console.log('Wallet Balance is',walletballance)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+const main = async() => {
+    await getWalletBalance()
+}
+
+main()
