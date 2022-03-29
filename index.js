214e1a1780c43d050170770a71a744f37414caab
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-03-29 11:05:49 -0400

    write function that airdrops Sol to desired wallet

--- index.js
+++ index.js
@@ -15,7 +15,19 @@ const getWalletBalance = async() => {
     }
 }
 
+const airDropSol = async() => {
+    try{
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
+        await connection.confirmTransaction(fromAirDropSignature)
+    } catch(err){
+        console.log(err)
+    }
+}
+
 const main = async() => {
+    await getWalletBalance()
+    await airDropSol()
     await getWalletBalance()
 }
 
